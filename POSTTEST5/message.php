52eabb34e1c62a0c77e3bd029af4f77d8f28a2d6
iiim1 <162287433+iiim1@users.noreply.github.com>
2024-10-06 20:40:10 +0800

    Add files via upload

---
+++ POSTTEST5/message.php
@@ -0,0 +1,57 @@
+<?php
+    include "koneksi.php";
+    session_start();
+    if (!isset($_SESSION['login'])) {
+        header('location:index.php');
+    }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <?php include('partials/head.php') ?>
+<body>
+    <?php include('partials/navbar.php') ?>
+    <div class="page-wrapper">
+        <main class="cushion-management">
+            <div class="container">
+                <h1 class="page-title">Message Management</h1>
+                <!-- <div class="action-bar">
+                    <a href="tambahCushion.php" class="btn">Add Cushion</a>
+                </div> -->
+                <div class="table-responsive">
+                    <?php 
+                        $data = mysqli_query($conn, "select * from tb_contact");?>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>No.</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Message</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $no=0;
+                            while($d = mysqli_fetch_array($data)){
+                                $no++;
+                            ?>
+                            <tr>
+                                <td><?= $no; ?></td>
+                                <td><?= $d["name"]; ?></td>
+                                <td><?= $d["email"]; ?></td>
+                                <td><?= $d["message"]; ?></td>
+                                <td><a href=editMessage.php?id=<?=$d['id'];?> name="edit" class="btn btn-sm btn-secondary">Edit</a>
+                                <a href=deleteMessage.php?id=<?=$d['id'];?> name="delete" class="btn btn-sm btn-danger" onclick="return confirm('Are you sure you want to delete this item?')">Delete</a>
+                                </td>
+                            </tr>
+                        <?php } ?>
+                        </tbody>
+		            </table>
+                </div>
+            </div>
+        </main>
+    </div>
+</body>
+</html>
---
+++ POSTTEST5/partials/navbar.php
@@ -0,0 +1,39 @@
+<?php
+    // session_start();
+
+?>
+<header>
+    <nav class="navbar">
+        <div class="navbar-container">
+            <div class="burger">
+                <div class="bar1"></div>
+                <div class="bar2"></div>
+                <div class="bar3"></div>
+            </div>
+            <?php if (isset($_SESSION['username']) && $_SESSION['username'] == 'admin') { ?>
+                <ul class="nav-links">
+                    <li><a href="cushion.php">Cushion</a></li>
+                    <li><a href="message.php">Message</a></li>
+                </ul>
+            <?php } else {?>
+                <ul class="nav-links">
+                    <li><a href="index.php#home">Home</a></li>
+                    <li><a href="index.php#products">Products</a></li>
+                    <li><a href="index.php#contact">Contact</a></li>
+                    <li><a href="biodata.php">About Me</a></li>
+                </ul>
+            <?php } ?>
+            <div class="navbar-right">
+                <button id="lighttheme" aria-label="Toggle light theme">
+                    <img id="light" src="assets/sun.png" alt="Light theme toggle" id="theme-icon">
+                </button>
+                <?php
+                    if (!isset($_SESSION['login']) || $_SESSION['login'] !== 'OK') { ?>
+                        <a href="login.php" class="btn-login">Login</a>
+                <?php } else {?>
+                    <a href="logout.php" class="btn-logout">Logout</a>
+                <?php }?>
+            </div>
+        </div>
+    </nav>
+</header>    
